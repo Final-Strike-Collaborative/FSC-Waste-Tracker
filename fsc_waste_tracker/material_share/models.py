3f33ca5255b0e65de0037d72e4e046bb35f3bb19
William Sturgiss <william.sturgiss@protonmail.com>
2025-03-08 11:49:00 -0500

    start adding templates and pages for posts/comments

--- fsc_waste_tracker/material_share/models.py
+++ fsc_waste_tracker/material_share/models.py
@@ -9,7 +9,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
 
     def __str__(self):
-        return self.title
+        return f"{self.author} posted: {self.title}"
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
--- fsc_waste_tracker/material_share/views.py
+++ fsc_waste_tracker/material_share/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from django.template import loader
+from .models import Post
 
+def index(request):
+    latest_posts = Post.objects.order_by('-created_at')[:5]
+    context = { "latest_posts": latest_posts }
+    return render(request, "material_share/index.html", context)
+
+def new_post(request):
+    return HttpResponse("Post a new post here")
+
+def view_post(request, post_id):
+    context = { "post": get_object_or_404(Post, pk=post_id) }
+    return render(request, "material_share/view_post.html", context)
 # Create your views here.
